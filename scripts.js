4fd29e209550271966da2190f000cb84bd5c17fd
HorvathSandor91 <you@example.com>
2020-04-16 18:33:19 +0200

    javaScript

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let name = 'Sanyi';
+//backstick shorcut
+console.log(`Hello, my dear ${name}`);
+
+//JQuery par
+
+$('p').text('The JQuery part has started!');
+
+$('h1').css('background', 'red');
+
+$('h2').toggleClass('.green');
+$('p').toggleClass('.green');
+
+$('main').append('<p>Added element</p>');
