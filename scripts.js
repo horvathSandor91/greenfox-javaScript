42da8a0dd893152222c76ef877513bcf212a71a7
HorvathSandor91 <you@example.com>
2020-04-17 19:54:32 +0200

    JS with JQuery

--- scripts.js
+++ scripts.js
@@ -18,11 +18,52 @@ $('main').append('<p id="second">Added second element</p>');
 $('#first').remove();
 
 $('#add-button').click(() => {
-    $('main').append('<p>Added a new p tag</p>');
+    if ($('main p').length < 5) {;
+        $('main').append(`<p> Username: ${$('#name').val()}</p>`);
+    } else {
+        $('main').append('<p class ="green">Added a new p tag</p>');
+    }
 });
+
 
 let removeFunction = () => {
     $('main p:last-child').remove();
 }
 
 $('#remove-button').click(removeFunction);
+
+// Conditions and Loops
+
+let age = 33;
+
+if (age > 35) {
+    console.log('youre too old');
+
+} else {
+    console.log("You're young");
+}
+
+
+
+// $('main').append('<div class="box"></div>');
+// $('main div:last-child').css('background', 'orange');
+// $('main').append('<div class="box"></div>');
+// $('main div:last-child').css('background', 'yellow');
+// $('main').append('<div class="box"></div>');
+// $('main div:last-child').css('background', 'purple');
+// $('main').append('<div class="box"></div>');
+// $('main div:last-child').css('background', 'blue');
+// $('main').append('<div class="box"></div>');
+// $('main div:last-child').css('background', 'green');
+
+let colors = ['orange', 'yellow', 'purple', 'blue', 'green'];
+colors.push('magenta');
+
+colors.forEach((color) => {
+    $('main').append('<div class="box"></div>');
+    $('main div:last-child').css('background', color);
+});
+
+for (let i = 0; i < 100; i++) {
+    console.log('current number:' + i);
+};
