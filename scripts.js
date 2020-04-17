218de2465db49e62427c791f8bf16e18f1c36c67
HorvathSandor91 <you@example.com>
2020-04-17 16:24:11 +0200

    JQuery part

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
 let name = 'Sanyi';
-//backstick shorcut
+//backtick shorcut
 console.log(`Hello, my dear ${name}`);
 
-//JQuery par
 
-$('p').text('The JQuery part has started!');
+//JQuery part
+
+$('p').html('The JQuery part has started!');
 
 $('h1').css('background', 'red');
 
-$('h2').toggleClass('.green');
-$('p').toggleClass('.green');
+$('h2').toggleClass('green');
+$('p').toggleClass('green');
 
-$('main').append('<p>Added element</p>');
+$('main').append('<p id="first">Added element</p>');
+$('main').append('<p id="second">Added second element</p>');
+
+$('#first').remove();
+
+$('#add-button').click(() => {
+    $('main').append('<p>Added a new p tag</p>');
+});
+
+let removeFunction = () => {
+    $('main p:last-child').remove();
+}
+
+$('#remove-button').click(removeFunction);
